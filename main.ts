54e867826c8b755cfb8010efc6a5f7ab2200dd4c
Andy Clyde <a.clyde@elsevier.com>
2016-12-13 12:49:33 +0000

    Day 12 working !

--- main.ts
+++ main.ts
@@ -8,6 +8,14 @@ export interface Registers {
     d: number;
 }
 
+export interface Instruction {
+    command: string;
+    offset: number;
+    increment: number;
+    register: string;
+    source: string;
+}
+
 let cpyRE = new RegExp(/cpy ([0-9]+) ([a-z]+)/);
 let cpyFromRegisterRE = new RegExp(/cpy ([a-z]+) ([a-z]+)/);
 let incRE = new RegExp(/inc ([a-z]+)/);
@@ -15,48 +23,86 @@ let decRE = new RegExp(/dec ([a-z]+)/);
 let jmpRE = new RegExp(/jnz ([a-z]+) ([-0-9]+)/);
 let jmpStaticRE = new RegExp(/jnz ([0-9]+) ([-0-9]+)/);
 
-function processInstructions(instructions: string[], registers: Registers): Registers {
-    while (registers.next < instructions.length) {
-        let instruction = instructions[registers.next];
+function processInstructions(rawInstructions: string[], registers: Registers): Registers {
+    let instructions: Instruction[] = rawInstructions.map((rawInstruction) => {
         let amount: number;
         let register: string;
-        let offset: number;
-        offset = 1;
-        console.log(registers.a);
-        if (instruction.indexOf('cpy') === 0) {
-            let parts = cpyRE.exec(instruction);
+        let offset: number = 1;
+        let parts: any[];
+        let command: string;
+        let source: string = '';
+        if (rawInstruction.indexOf('cpy') === 0) {
+            command = 'cpy';
+            parts = cpyRE.exec(rawInstruction);
             if (parts) {
                 amount = parseInt(parts[1], 10);
                 register = parts[2];
             } else {
-                parts = cpyFromRegisterRE.exec(instruction);
-                amount = registers[parts[1]];
+                parts = cpyFromRegisterRE.exec(rawInstruction);
+                amount = 0;
+                source = parts[1];
                 register = parts[2];
             }
-        } else if (instruction.indexOf('inc') === 0) {
-            let parts = incRE.exec(instruction);
+        } else if (rawInstruction.indexOf('inc') === 0) {
+            command = 'inc';
+            parts = incRE.exec(rawInstruction);
             register = parts[1];
             amount = 1;
-        } else if (instruction.indexOf('dec') === 0) {
-            let parts = decRE.exec(instruction);
+        } else if (rawInstruction.indexOf('dec') === 0) {
+            command = 'dec';
+            parts = decRE.exec(rawInstruction);
             register = parts[1];
             amount = -1;
-        } else if (instruction.indexOf('jnz') === 0) {
-            let parts = jmpRE.exec(instruction);
+        } else if (rawInstruction.indexOf('jnz') === 0) {
+            command = 'jnz';
+            parts = jmpRE.exec(rawInstruction);
             if (parts) {
                 register = parts[1];
-                offset = (registers[register] > 0) ? parseInt(parts[2], 10) : 1;``
+                offset = parseInt(parts[2], 10);
                 amount = 0;
             } else {
-                parts = jmpStaticRE.exec(instruction);
+                parts = jmpStaticRE.exec(rawInstruction);
                 register = 'a';
-                let value = parseInt(parts[1], 10);
-                offset = (value > 0) ? parseInt(parts[2], 10) : 1;
-                amount = 0;
+                offset = parseInt(parts[2], 10);
+                amount = parseInt(parts[1], 10);
             }
         }
-        registers[register] += amount;
-        registers.next += offset;
+        return <Instruction>{
+            command: command,
+            offset: offset,
+            increment: amount,
+            register: register,
+            source: source
+        };
+    });
+    console.log(instructions);
+
+    let instruction: Instruction;
+    while (registers.next < instructions.length) {
+        instruction = instructions[registers.next];
+        if (instruction.command === 'cpy') {
+            let value = instruction.increment;
+            if (instruction.source) {
+                value = registers[instruction.source];
+            }
+            registers[instruction.register] = value;
+            registers.next += instruction.offset;
+        } else if (instruction.command !== 'jnz') {
+            registers[instruction.register] += instruction.increment;
+            registers.next += instruction.offset;
+        } else {
+            let value = 0;
+            if (instruction.increment === 0) {
+                value = registers[instruction.register];
+            } else {
+                value = instruction.increment;
+            }
+            if (value > 0) {
+                registers.next += instruction.offset;
+            } else {
+                registers.next += 1;
+            }
+        }
     }
     return registers;
 }
@@ -86,6 +132,6 @@ const instructions = [
     'dec c',
     'jnz c -5',
 ];
-let registers = processInstructions(instructions, { next: 0, a: 0, b: 0, c: 0, d: 0 });
+let registers = processInstructions(instructions, { next: 0, a: 0, b: 0, c: 1, d: 0 });
 
 console.log(registers.a);
