d9e0f99d3f73179ea7956f785e580144eb7a4f6f
Andy Clyde <me@andyclyde.com>
2016-12-20 06:21:17 +0000

    2nd part still not working

--- src/day19/day19.ts
+++ src/day19/day19.ts
@@ -1,31 +1,26 @@
 'use strict';
 
-interface Elf {
-    id: number;
-    presents: number;
-}
 
 export const elephantParty = (numElves: number): number => {
     let thisElf = 0;
-    let presents: Elf[] = [];
+    let presents: number[] = [];
     for (let i = 1; i <= numElves; i++) {
-        presents.push({ id: i, presents: 1 });
+        presents.push(i);
     }
     let offset: number;
     while (true) {
         if (presents[thisElf]) {
-            if (presents[thisElf].presents === numElves) {
-                return presents[thisElf].id;
+            if (presents.length === 1) {
+                return presents[0];
             }
             offset = Math.floor(presents.length / 2);
             let targetElf = (thisElf + offset) % presents.length;
-            presents[thisElf].presents += presents[targetElf].presents;
-            presents = presents.slice(0, targetElf - 1).concat(presents.slice(targetElf + 1));
+            console.log(presents[thisElf], presents[targetElf]);
+            presents.splice(targetElf, 1);
         }
         thisElf = (thisElf + 1);
         if (thisElf > presents.length) {
             thisElf = 0;
         }
-        console.log(presents.length);
     }
 };
