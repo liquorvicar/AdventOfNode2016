4cef0ddd310fea3a3d74d501b31d351827026f86
Andy Clyde <me@andyclyde.com>
2016-12-27 12:47:03 +0000

    Day 22 part 2 [failed attempt]

--- src/day22/day22.ts
+++ src/day22/day22.ts
@@ -36,7 +36,6 @@ export const findPairs = (disks: Disk[]): Disk[][] => {
     for (let diskA of disks) {
         for (let diskB of disks) {
             if (diskA.used > 0 && diskA.used <= diskB.avail && (diskA.x != diskB.x || diskA.y != diskB.y)) {
-                console.log(diskA, diskB);
                 pairs.push([
                     diskA,
                     diskB
@@ -49,3 +48,76 @@ export const findPairs = (disks: Disk[]): Disk[][] => {
     }
     return pairs;
 };
+
+export const findShortestWayToGetData = (source: Disk, target: Disk, disks: Disk[]): number => {
+    let minSteps = 0;
+    let emptyDisk = findEmptyDisk(disks);
+    minSteps += moveEmptyNextToDisk(disks, emptyDisk, target);
+    minSteps += moveTargetDataNextToSource(disks, target, source);
+    minSteps += moveEmptyNextToDisk(disks, emptyDisk, source);
+    return minSteps + 1;
+};
+
+const findEmptyDisk = (disks: Disk[]): Disk => {
+    return this.disks.find((disk) => disk.used === 0);
+};
+
+const moveEmptyNextToDisk = (disks: Disk[], emptyDisk: Disk, target: Disk, moves: string[], minSteps: number): number => {
+    if ((emptyDisk.x === target.x && Math.abs(emptyDisk.y - target.y) === 1)
+        || (emptyDisk.y === target.y && Math.abs(emptyDisk.x - target.x) === 1)) {
+        return (minSteps > moves.length || minSteps === 0) ? moves.length : minSteps;
+    }
+    if (minSteps > 0 && moves.length > minSteps) {
+        return minSteps;
+    }
+    let possibleMoves = findPossibleMoves(emptyDisk, target);
+    possibleMoves.forEach((possibleMove) => {
+        if (moves.indexOf(`${possibleMove.x}+${possibleMove.y}`) >= 0) {
+            return;
+        }
+        let newMoves = JSON.parse(JSON.stringify(moves));
+        newMoves.push(`${possibleMove.x}+${possibleMove.y}`);
+        emptyDisk.used += possibleMove.used;
+        emptyDisk.avail -= possibleMove.used;
+        possibleMove.used = 0;
+        minSteps = this.moveEmptyNextToDisk()
+    });
+    return 0;
+};
+
+const moveTargetDataNextToSource = (target: Disk, source: Disk): number => {
+    return 0;
+};
+
+const findPossibleMoves = (current: Disk, target: Disk): Disk[] => {
+    let possibleMoves = [];
+    let left = this.findDisk(current.x - 1, current.y);
+    if (left && left.avail <= current.used) {
+        possibleMoves.push(left);
+    }
+    let right = this.findDisk(current.x + 1, current.y);
+    if (right && right.avail <= current.used) {
+        possibleMoves.push(right);
+    }
+    let up = this.findDisk(current.x, current.y - 1);
+    if (up && up.avail <= current.used) {
+        possibleMoves.push(up);
+    }
+    let down = this.findDisk(current.x, current.y + 1);
+    if (down && down.avail <= current.used) {
+        possibleMoves.push(down);
+    }
+    return possibleMoves.sort((diskA, diskB) => {
+        let distanceA = Math.abs(diskA.x - target.x) + Math.abs(diskA.y - target.y);
+        let distanceB = Math.abs(diskB.x - target.x) + Math.abs(diskB.y - target.y);
+        if (distanceA < distanceB) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+};
+
+const findDisk = (x: number, y: number): Disk => {
+    return this.disks.find(disk => disk.x === x && disk.y === y);
+}
